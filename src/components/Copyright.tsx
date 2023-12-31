5601ec6879aa83659ee08d0508662fafe79397a6
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2023-12-31 17:41:41 +0100

    Base

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,8 +1,10 @@
-import './Copyright.css'
+import './Copyright.css';
 
 export const Copyright: React.FC = () => (
-  <footer className='copyright'>
-    <h4>Curso de React desde cero ⚛️ － <span>@midudev</span></h4>
-    <h5>Creando un TODO con TypeScript</h5>
-  </footer>
-)
+    <footer className='copyright'>
+        <h4>
+            Marc C. Avila ⚛️ － <span>@midudev</span>
+        </h4>
+        <h5>Creando un TODO con TypeScript</h5>
+    </footer>
+);
